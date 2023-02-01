d4557dc54672f28690eb74312aa0a25aa9940f91
262882 <2628829@students.wits.ac.za>
2023-02-01 15:30:04 +0200

    Configure build process

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # myDetectorEvaluation
+cd myDetectorEvaluation
+cmake .
+make
--- run_test.cpp
+++ run_test.cpp
@@ -1,9 +1,45 @@
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
 #include <iostream>
 
-using namespace::std;
+using namespace std;
+using namespace cv;
 
 int main(){
-    cout<<"Hello World"<<endl;
+    
+    cout << "Hello";
+    // Create a VideoCapture object and open the input file
+    // If the input is the web camera, pass 0 instead of the video file name
+    VideoCapture video("./tracking_test0.avi");  //Name must include a number
+    cout<<"Open";
+
+    // Check if camera opened successfully
+    //if(!video.isOpened()){
+    //    cout << "Error opening video stream or file" << endl;
+    //    return -1;
+    //    }
+   
+  /*while(true){
+ 
+    Mat frame;
+    // Capture frame-by-frame
+    cap >> frame;
+  
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
+ 
+    // Display the resulting frame
+    imshow( "Frame", frame );
+ 
+    // Press  ESC on keyboard to exit
+    char c=(char)waitKey(25);
+    if(c==27)
+      break;
+  }*/
+  
+    video.release(); // When everything done, release the video capture object
+    //destroyAllWindows(); // Closes all the frames
+   
     return 0;
-}
+    }
+
