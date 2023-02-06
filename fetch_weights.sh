8c0469ad76255beecc5a5dafe76a51555dad7d4f
262882 <2628829@students.wits.ac.za>
2023-02-06 22:47:00 +0200

    Update network weights downloader

--- fetch_weights.sh
+++ fetch_weights.sh
@@ -6,8 +6,10 @@ cd net
 echo "Retrieving Viola Jones weights"
 wget https://github.com/kipr/opencv/raw/master/data/haarcascades/haarcascade_frontalface_default.xml
 
-echo "Retrieving config files"
-wget https://github.com/pjreddie/darknet/raw/master/cfg/yolov3-tiny.cfg
+echo "Retrieving mobile ssd files"
+wget https://github.com/PINTO0309/MobileNet-SSD-RealSense/raw/master/caffemodel/MobileNetSSD/MobileNetSSD_deploy.prototxt
+wget https://github.com/PINTO0309/MobileNet-SSD-RealSense/raw/master/caffemodel/MobileNetSSD/MobileNetSSD_deploy.caffemodel
 
-echo "Retrieving yolo-tiny weights"
+echo "Retrieving yolo-tiny files"
+wget https://github.com/pjreddie/darknet/raw/master/cfg/yolov3-tiny.cfg
 wget https://pjreddie.com/media/files/yolov3-tiny.weights
--- run_test_mobilessd.py
+++ run_test_mobilessd.py
@@ -4,14 +4,13 @@
 import time
 import sys
 import cv2
-import numpy as np
 
 print("Welcome to the Object Detector Tester")
 run_time = time.perf_counter()
 
 print("Initialising detector")
-detector = cv2.dnn.readNetFromCaffe('net/MobileNetSSD_deploy.prototxt', 'net/MobileNetSSD_deploy.caffemodel')
-#cv2.dnn.readNetFromDarknet('./net/yolov3-tiny.cfg', './net/yolov3-tiny.weights')
+detector = cv2.dnn.readNetFromCaffe('net/MobileNetSSD_deploy.prototxt',
+                                    'net/MobileNetSSD_deploy.caffemodel')
 detector.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 detector.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 print("Detector ready")
@@ -30,8 +29,8 @@ while cap.isOpened():
 
     if ret:
         blob = cv2.dnn.blobFromImage(frame, scalefactor=1/255,
-                                      size = (300, 300), # Resolution multiple of 32
-                                      swapRB=True, crop=True)
+                                     size=(300, 300), # Resolution multiple of 32
+                                     swapRB=True, crop=True)
         detector.setInput(blob)
         layer_output = detector.forward()
 
@@ -39,37 +38,37 @@ while cap.isOpened():
         rate_fps = 1/(time.perf_counter()-run_time)
         run_time = time.perf_counter()
         frame = cv2.putText(frame, str(round(rate_fps))+" FPS",
-                           ((frame.shape[1]//10)*7,(frame.shape[0]//10)*9), font, 1, color)
+                            ((frame.shape[1]//10)*7, (frame.shape[0]//10)*9), font, 1, color)
 
         #Size of frame resize (300x300)
         cols = 300
         rows = 300
 
-        for detection in layer_output[0,0]:
-            confidence = detection[2] 
+        for detection in layer_output[0, 0]:
+            confidence = detection[2]
             class_id = int(detection[1])
-            if confidence > 0.2 and class_id == 15:  # Threshold       
-                 
-                # Object location 
-                xLeftBottom = int(detection[3] * cols) 
+            if confidence > 0.2 and class_id == 15:  # Threshold
+
+                # Object location
+                xLeftBottom = int(detection[3] * cols)
                 yLeftBottom = int(detection[4] * rows)
-                xRightTop   = int(detection[5] * cols)
-                yRightTop   = int(detection[6] * rows)
-                
+                xRightTop = int(detection[5] * cols)
+                yRightTop = int(detection[6] * rows)
+
                 # Factor for scale to original size of frame
                 heightFactor = frame.shape[0]/rows
                 widthFactor = frame.shape[1]/cols
                 # Scale object detection to frame
-                xLeftBottom = int(widthFactor * xLeftBottom) 
+                xLeftBottom = int(widthFactor * xLeftBottom)
                 yLeftBottom = int(heightFactor * yLeftBottom)
-                xRightTop   = int(widthFactor * xRightTop)
-                yRightTop   = int(heightFactor * yRightTop)
-                # Draw location of object  
+                xRightTop = int(widthFactor * xRightTop)
+                yRightTop = int(heightFactor * yRightTop)
+                # Draw location of object
                 frame = cv2.rectangle(frame, (xLeftBottom, yLeftBottom), (xRightTop, yRightTop),
-                              (0, 255, 0),3)
+                                      (0, 255, 0), 3)
 
         # Display the resulting frame
-        cv2.imshow('Frame',frame)
+        cv2.imshow('Frame', frame)
 
         # Press Q on keyboard to  exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
